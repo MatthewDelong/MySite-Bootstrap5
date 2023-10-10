3fa4c39824a39490c68ca8c1557239ff19d59f4f
Matthew Delong <matthew.delong@hotmail.co.uk>
2023-10-10 12:24:05 +0100

    renamed weather to old, added new weather

--- js/weather.js
+++ js/weather.js
@@ -1,19 +1,45 @@
-const wrapper = document.querySelector(".wrapper"),
-inputPart = document.querySelector(".input-part"),
-infoTxt = inputPart.querySelector(".info-txt"),
-inputField = inputPart.querySelector("input"),
-locationBtn = inputPart.querySelector("button"),
-weatherPart = wrapper.querySelector(".weather-part"),
-wIcon = weatherPart.querySelector("img"),
-arrowBack = wrapper.querySelector("header i");
+let weather = {
+    "apikey": '0cddfd47649d15c0808350667e99c538',
+    fetchWeather : function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&units=metric&appid=" 
+        + this.apikey)
+        .then((Response) => Response.json())
+        .then((data) => this.displayWeather(data));
+    },
 
-let api;
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "" + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+    },
 
-inputField.addEventListener("keyup", e =>{
-    if(e.key == "Enter" && inputField.value != ""){
-        requestApi(inputField.value);
+    search : function() {
+        this.fetchWeather(document.querySelector(".searchbar").value);
+    }
+};
+
+document.querySelector(".search button_1").addEventListener("click", function() {
+    weather.search();
+});
+
+document.querySelector(".searchbar").addEventListener("keyup", function(event){
+    if(event.key == "Enter"){
+        weather.search();
     }
 });
+
+weather.fetchWeather("London,UK");
 locationBtn.addEventListener("click", () =>{
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(onSuccess, onError);
@@ -22,67 +48,6 @@ locationBtn.addEventListener("click", () =>{
     }
 });
 
-function requestApi(city){
-    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=0cddfd47649d15c0808350667e99c538`;
-    fetchData();
+function speedConverter(valNum) {
+    document.getElementById("outputMPH").innerHTML=valNum/1.609344;
 }
-
-function onSuccess(position){
-    const {latitude, longitude} = position.coords;
-    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=0cddfd47649d15c0808350667e99c538`;
-    fetchData();
-}
-
-function onError(error){
-    infoTxt.innerText = error.message;
-    infoTxt.classList.add("error");
-}
-
-function fetchData(){
-    infoTxt.innerText = "Getting weather details...";
-    infoTxt.classList.add("pending");
-    fetch(api).then(res => res.json()).then(result => weatherDetails(result)).catch(() =>{
-        infoTxt.innerText = "Something went wrong";
-        infoTxt.classList.replace("pending", "error");
-    });
-}
-
-function weatherDetails(info){
-    if(info.cod == "404"){
-        infoTxt.classList.replace("pending", "error");
-        infoTxt.innerText = `${inputField.value} isn't a valid city name`;
-    }else{
-        const city = info.name;
-        const country = info.sys.country;
-        const {description, id} = info.weather[0];
-        const {temp, feels_like, humidity} = info.main;
-
-        if(id == 800){
-            wIcon.src = "/images/icons/clear.svg";
-        }else if(id >= 200 && id <= 232){
-            wIcon.src = "/images/icons/storm.svg";  
-        }else if(id >= 600 && id <= 622){
-            wIcon.src = "/images/icons/snow.svg";
-        }else if(id >= 701 && id <= 781){
-            wIcon.src = "/images/icons/haze.svg";
-        }else if(id >= 801 && id <= 804){
-            wIcon.src = "/images/icons/cloud.svg";
-        }else if((id >= 500 && id <= 531) || (id >= 300 && id <= 321)){
-            wIcon.src = "/images/icons/rain.svg";
-        }
-        
-        weatherPart.querySelector(".temp .numb").innerText = Math.floor(temp);
-        weatherPart.querySelector(".weather").innerText = description;
-        weatherPart.querySelector(".location span").innerText = `${city}, ${country}`;
-        weatherPart.querySelector(".temp .numb-2").innerText = Math.floor(feels_like);
-        weatherPart.querySelector(".humidity span").innerText = `${humidity}%`;
-        infoTxt.classList.remove("pending", "error");
-        infoTxt.innerText = "";
-        inputField.value = "";
-        wrapper.classList.add("active");
-    }
-}
-
-arrowBack.addEventListener("click", ()=>{
-    wrapper.classList.remove("active");
-});
