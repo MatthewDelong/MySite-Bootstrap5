c3a1a3d02919c398f5d2bd7e6f28dee0f4964135
MatthewDelong <matthew.delong@hotmail.co.uk>
2024-12-22 17:42:40 +0000

    added new files & index_new to implement Owl Carousel

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+    $(".owl-carousel").owlCarousel({
+        autoplay: true,
+        autoplayhoverpause: true,
+        autoplaytimeout: 100,
+        items: 1,
+        singleItem: true,
+        nav: false,
+        loop: true
+});
