74b6459ca4d2ffa31f06da77a019bab2a406366d
MatthewDelong <matthew.delong@hotmail.co.uk>
2024-12-23 15:50:10 +0000

    Slider- added navigation and made responsive

--- js/script.js
+++ js/script.js
@@ -1,10 +1,37 @@
-
-    $(".owl-carousel").owlCarousel({
+     $(".owl-carousel").owlCarousel({
         autoplay: true,
         autoplayhoverpause: true,
         autoplaytimeout: 100,
-        items: 1,
-        singleItem: true,
-        nav: false,
-        loop: true
+        items: 6,
+        singleItem: 1,
+        nav: true,
+        loop: true,
+        responsiveClass: true,
+        responsive: {
+            0 : {
+                items: 1,
+                dots: false,
+                nav: false
+            },
+            485: {
+                items: 2,
+                dots: false,
+                nav: false
+            },
+            728: {
+                items: 3,
+                dots: false,
+                nav: true
+            }, 
+            960: {
+                items: 4,
+                dots: false,
+                nav: true
+            },
+            1200: {
+                items: 5,
+                dots: false,
+                nav: true
+            }
+        }
 });
