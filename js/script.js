a596e6425d85f9e5e31424eac262fd6e0c3be3ad
MatthewDelong <matthew.delong@hotmail.co.uk>
2024-12-24 12:17:16 +0000

    changed script.js to show 4  slides from 5
    This looks better to me on a desktop

--- js/script.js
+++ js/script.js
@@ -1,36 +1,33 @@
-     $(".owl-carousel").owlCarousel({
-        autoplay: true,
-        autoplayhoverpause: true,
-        autoplaytimeout: 100,
-        items: 6,
-        nav: true,
-        loop: true,
-        responsiveClass: true,
-        responsive: {
-            0 : {
-                items: 1,
-                dots: false,
-                nav: false
-            },
-            485: {
-                items: 2,
-                dots: false,
-                nav: false
-            },
-            728: {
-                items: 3,
-                dots: false,
-                nav: true
-            }, 
-            960: {
-                items: 4,
-                dots: false,
-                nav: true
-            },
-            1200: {
-                items: 5,
-                dots: false,
-                nav: true
-            }
-        }
+/* To use Animate/Transistions then use script-with-animate and change at the bottom of index.html */
+
+$(".owl-carousel").owlCarousel({
+  autoplay: true,
+  autoplayhoverpause: true,
+  autoplaytimeout: 100,
+  items: 3,
+  nav: true,
+  loop: true,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      dots: false,
+      nav: false,
+    },
+    485: {
+      items: 2,
+      dots: false,
+      nav: false,
+    },
+    728: {
+      items: 3,
+      dots: false,
+      nav: true,
+    },
+    960: {
+      items: 4,
+      dots: false,
+      nav: true,
+    },
+  },
 });
